4ff50cfe17705c5e0a6ac753548a078512228074
Adnan Raja <58033249+adnannrajaa@users.noreply.github.com>
2020-02-27 22:23:33 +0500

    2/27/2020/10:23PM

---
+++ assets/js/tawk-chat.js
@@ -0,0 +1,11 @@
+// <!--Start of Tawk.to Script-->
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+let s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5e56c88fa89cda5a18881a82/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+// <!--End of Tawk.to Script-->
